a64c07fe8a413c57f1a7b95260e47535bf7e7e29
Dennis <31170537+llecram@users.noreply.github.com>
2018-01-11 22:46:55 -0500

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void prime(int a,int b=2){
+    if(b==a){
+        cout<<"el numero es primo";
+    }
+    else{
+        if(a%b==0 || a==1){
+            cout<<"el numero no es primo";
+            return;
+        }
+        if(a>=2){
+            prime(a,++b);
+        }
+    }
+}
+int main(){
+    int m;
+    cout<<"ingrese el numero a evaluar";
+    cin>>m;
+    prime(m);
+
+}
